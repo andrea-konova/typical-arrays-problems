82bcdb1286c5a6c2fb6bed7fe2e1ddfd8ed8ed89
Andrii Konovalenko <an256.ak@gmail.com>
2020-09-30 14:53:14 +0300

    feat: created part of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!arguments) {
+    return 0;
+  } else if (array.length > 0) {
+    return Math.min(...array);
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments || arguments.length > 0) {
+    return Math.max.apply(null, array);
+  } else {
+    return 0;
+  }
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  // return 0;
+  let sum = array.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+  }, 0);
+  let arrayLength = array.length;
+  return sum / arrayLength;
 }
