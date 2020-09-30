328f08677660517bccf5eef44b4b1516660a7832
Andrii Konovalenko <an256.ak@gmail.com>
2020-09-30 15:34:14 +0300

    fix: fixed errors

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 
 exports.min = function min (array) {
-  if (!arguments) {
+  if (!array || array.length === 0) {
     return 0;
-  } else if (array.length > 0) {
+  } else {
     return Math.min(...array);
   }
-  
 }
 
 exports.max = function max (array) {
-  if (arguments || arguments.length > 0) {
-    return Math.max.apply(null, array);
-  } else {
+  if (!array || array.length === 0) {
     return 0;
+  } else {
+    return Math.max.apply(null, array);
   }
-  
 }
 
 exports.avg = function avg (array) {
-  // return 0;
-  let sum = array.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue;
-  }, 0);
-  let arrayLength = array.length;
-  return sum / arrayLength;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let sum = array.reduce(function (accumulator, currentValue) {
+      return accumulator + currentValue;
+    }, 0);
+    let arrayLength = array.length;
+    return sum / arrayLength;
+  }
 }
+
+
